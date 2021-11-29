4be1ae17ca4760d4362f1bd414dd8e5126ea1877
GyeongTak Byeon <bkt1214bkt@naver.com>
2021-11-29 10:08:54 +0900

    all edit

---
+++ script/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#footerWrap #facebookIcon').click(function(){
+        window.open("https://facebook.com");
+    });
+    $('#footerWrap #twitterIcon').click(function(){
+        window.open("https://twitter.com");
+    });
+    $('#footerWrap #instagramIcon').click(function(){
+        window.open("https://instagram.com");
+    });
+});
