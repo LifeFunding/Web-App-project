ce55ad93112af78f675616c029963644a9877c8e
jaesukpark77 <jaesukpark77@naver.com>
2021-11-30 10:03:59 +0900

    recommend js 추가
    추천페이지 js 추가

--- script/recommend.js
+++ script/recommend.js
@@ -11,4 +11,16 @@ $(document).ready(function(){
     $('#headerWrap #titleBox #join').click(function(){
         window.location.href="../html/join.html";
     });
+    $('#recommend #finish').click(function(){
+        window.location.href="../html/home.html";
+    });
+    $('#recommend #sns #facebookIcon').click(function(){
+        window.open("https://facebook.com");
+    });
+    $('#recommend #sns #twitterIcon').click(function(){
+        window.open("https://twitter.com");
+    });
+    $('#recommend #sns #instagramIcon').click(function(){
+        window.open("https://instagram.com");
+    });
 });
