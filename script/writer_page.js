d93116be4bb795ddc808aa7064661a38c640e9fa
jaesukpark77 <jaesukpark77@naver.com>
2021-12-01 08:22:26 +0900

    writer_page js 수정
    펀딩페이지로 이동 기능 추가

--- script/writer_page.js
+++ script/writer_page.js
@@ -5,5 +5,14 @@ $(document).ready(function(){
     $('#headerWrap #titleBox #mypage').click(function(){
         window.location.href="../html/mypage.html";
     });
+    $('#headerWrap #titleBox #login').click(function(){
+        window.location.href="../html/login.html";
+    });
+    $('#headerWrap #titleBox #join').click(function(){
+        window.location.href="../html/join.html";
+    });
+    $('#my_list #wrapper_card #ex1').click(function(){
+        window.location.href="../html/lifepost.html";
+    });
    
 });
