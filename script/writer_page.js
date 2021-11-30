1c4a8d2f092a050131c6a642f96343169cfd187c
GyeongTak Byeon <bkt1214bkt@naver.com>
2021-12-01 01:43:09 +0900

    edit css/script

---
+++ script/writer_page.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#headerWrap #titleBox #title').click(function(){
+        window.location.href="../html/home.html";
+    });
+    $('#headerWrap #titleBox #mypage').click(function(){
+        window.location.href="../html/mypage.html";
+    });
+   
+});
